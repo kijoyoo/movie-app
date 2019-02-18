bcf9d9a8f7a7f00684377d2915afb15878748e8b
kijoyoo <yookijo897@gmail.com>
2019-02-18 19:48:16 +0900

    Initial commit

---
+++ src/Movie.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react' ;
+import './Movie.css';
+
+class Movie extends Component {
+    render(){
+        return(
+            <div>
+                <MoviePoster poster={this.props.poster}/>
+                <h3>{this.props.title}</h3>
+           </div>
+        )
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src= {this.props.poster}/>
+             )
+    }
+}
+export default Movie;
