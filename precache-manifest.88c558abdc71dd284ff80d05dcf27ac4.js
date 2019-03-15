976a758376e535dd45d739cf2d5e317ee0efeb52
kijoyoo <youkijo@daum.net>
2019-03-15 21:27:16 +0900

    Updates

---
+++ precache-manifest.88c558abdc71dd284ff80d05dcf27ac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.3a531502.js"
+  },
+  {
+    "revision": "ed6fcb210292283e4c4f",
+    "url": "/movie-app/static/js/main.ed6fcb21.chunk.js"
+  },
+  {
+    "revision": "df0b9ca0f81249544de6",
+    "url": "/movie-app/static/js/2.df0b9ca0.chunk.js"
+  },
+  {
+    "revision": "ed6fcb210292283e4c4f",
+    "url": "/movie-app/static/css/main.2035e113.chunk.css"
+  },
+  {
+    "revision": "da950656b43ea504a758a4ae4d4ea72c",
+    "url": "/movie-app/index.html"
+  }
+];
